b5130f5d2c4531bf17bc4a150ae6f160272b2b8e
caimeng <caaiimeenngg@163.com>
2024-08-01 15:12:46 +0800

    rabbitmq 消息应答

--- rabbitmq/src/main/java/com/example/rabbitmq/consumer/MessageConsumer.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/consumer/MessageConsumer.java
@@ -7,6 +7,7 @@ import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import lombok.SneakyThrows;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
@@ -42,6 +43,63 @@ public class MessageConsumer extends RabbitMQServiceAbs {
                 // 获取消息内容
                 String msg = new String(body, StandardCharsets.UTF_8);
                 System.out.println("【接收消息】" + msg);
+            }
+        };
+        // 连接队列与消费者
+        channel.basicConsume(QUEUE_NAME, consumer);
+    }
+
+    /**
+     * 自动应答ACK
+     */
+    @SneakyThrows
+    public void firstConsumerAutoACK() {
+        init();
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) {
+                // 在实际的开发中，在此方法内部需要进行业务接口的方法调用
+                try {
+                    // 每隔1秒消费一次
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.println("【" + count++ +"】消息的消费处理");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 获取消息内容
+                String msg = new String(body, StandardCharsets.UTF_8);
+                System.out.println("【接收消息】" + msg);
+            }
+        };
+        // 连接队列与消费者
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+
+    @SneakyThrows
+    public void firstConsumerManualACK() {
+        init();
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                // 在实际的开发中，在此方法内部需要进行业务接口的方法调用
+                try {
+                    // 每隔1秒消费一次
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.println("【" + count++ +"】消息的消费处理");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 获取消息内容
+                String msg = new String(body, StandardCharsets.UTF_8);
+                System.out.println("【接收消息】" + msg);
+                /*
+                 * 手工应答ACK
+                 * 参数 multiple 如果设置为false表示只对当前消息做应答
+                 * 如果设置为true，则表示对所有消息进行应答
+                 */
+                channel.basicAck(envelope.getDeliveryTag(), false);
             }
         };
         // 连接队列与消费者
--- rabbitmq/src/main/java/com/example/rabbitmq/package-info.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/package-info.java
@@ -6,7 +6,28 @@
  * <p>
  *     如果想要获取RabbitMQ的连接信息，则需要通过ConnectionFactory处理类来进行配置，
  *     在这个类中需要配置所有的连接信息，这个工厂类中提供了new Connection()的方法
+ * <p>
+ *     消息消费应答
+ *     <a href="https://www.bilibili.com/video/BV1kZ421N7k4/" />
+ *     如果消息没有被消费端正确的消费？
+ *          - 丢失
+ *          - 延迟
+ *          - 错误
+ *          - 重复
+ *     导致消费端没有收到消息，RabbitMQ里提供了应答机制：
+ *          只有消费端发送了应答数据之后，RabbitMQ才会执行消息的删除，
+ *          否则消息的重投处理。
+ *     如果某些消费端一直没有进行ACK应答信息回复的时候，那么RabbitMQ会重复地将已经消费的消息进行重新发送，
+ *          （将消息放回到发送队列中，进行重新投递）
+ *          从而可能造成内存泄漏，导致RabbitMQ服务宕机，所有需要进行ACK应答处理。
+ *          - 自动应答机制，通过channel提供的方法即可
+ *                  {@link com.example.rabbitmq.consumer.MessageConsumer#firstConsumerAutoACK()}
+ *                  {@link com.rabbitmq.client.Channel#basicConsume(String, boolean, Consumer)}
+ *          - 手工应答处理，主要是在Consumer类中完成
+ *                  {@link com.example.rabbitmq.consumer.MessageConsumer#firstConsumerManualACK()}
  * @author caimeng
  * @date 2024/7/31 16:16
  */
 package com.example.rabbitmq;
+
+import com.rabbitmq.client.Consumer;
