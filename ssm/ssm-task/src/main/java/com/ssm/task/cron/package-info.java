76cf39e08ac4f63670aa294f56919d93a8c16485
caimeng <caaiimeenngg@163.com>
2025-01-08 17:16:24 +0800

    SpringBatch CRON表达式

---
+++ ssm/ssm-task/src/main/java/com/ssm/task/cron/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * CRON 原本式一个Linux系统下执行定时任务的处理工具，利用如下的格式字符串来进行任务调度的安排:
+ * 格式一: 七个描述域
+ *      秒 分 时 日 月 周 年 (Seconds Minutes Hours DayOfMonth Month DayOfWeek Year)
+ * 格式一: 六个描述域
+ *      秒 分 时 日 月 周 (Seconds Minutes Hours DayOfMonth Month DayOfWeek)
+ *
+ * @author caimeng
+ * @date 2025/1/8 16:56
+ */
+package com.ssm.task.cron;
--- ssm/ssm-task/src/main/java/com/ssm/task/task/YootkTask.java
+++ ssm/ssm-task/src/main/java/com/ssm/task/task/YootkTask.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class YootkTask {
 
-    @Scheduled(fixedRate = 2000)    // 间隔两秒进行业务调度
+//    @Scheduled(fixedRate = 2000)    // 间隔两秒进行业务调度
+//    @Scheduled(cron = "* * * * * ?")    // 每秒进行一次调度。 ? 表示不生效的字段，仅适用于日和周
+//    @Scheduled(cron = "0 * * * * ?")    // 每分钟进行一次调度。 每次秒为0，即为每分钟。
+    @Scheduled(cron = "0/5 * * * * ?")    // 每5秒进行一次调度。 0开始，步进5
     public void runTask() {
         log.info("【YOOTK - 定时任务】当前时间: {}", System.currentTimeMillis());
     }
