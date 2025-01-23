140f2fcdc07c82f36e2d92e39f8e12e5a740a1e3
caimeng <caaiimeenngg@163.com>
2025-01-23 11:09:51 +0800

    SpringBoot,lombok请求对象转换

--- first-micro-boot/first-micro-boot-web/src/main/java/com/gardenia/web/action/MessageAction.java
+++ first-micro-boot/first-micro-boot-web/src/main/java/com/gardenia/web/action/MessageAction.java
@@ -1,9 +1,13 @@
 package com.gardenia.web.action;
 
+import com.gardenia.common.action.abs.AbstractBaseAction;
 import com.gardenia.web.service.IMessageService;
+import com.gardenia.web.vo.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController     // SpringMVC中的注解，基于Rest架构的处理
 @RequestMapping("/message/*")   // 添加父路径
-public class MessageAction {
+public class MessageAction extends AbstractBaseAction {     // 继承抽象类，完成字符串向日期型的转换
     @Autowired
     @Qualifier("iMessageServiceXmlImpl")
     private IMessageService iMessageService;
@@ -27,4 +31,23 @@ public class MessageAction {
     public String home() {
         return "Hello World!";
     }   // 响应的数据信息
+
+    /**
+     * 请求中含有Date类型参数，传参为字符串
+     * @param message 请求对象
+     * @return 修正后的请求对象
+     */
+    @PostMapping("/transStrToDate")    // 映射目录
+    public Object transStrToDate(@RequestBody Message message) {
+        message.setTitle("【ECHO】" + message.getTitle());
+        message.setContent("【ECHO】" + message.getContent());
+        /*
+         * {
+         *    "title": "【ECHO】消息标题",
+         *    "pubDate": "2025-01-23T00:00:00.000+00:00",
+         *    "content": "【ECHO】消息内容"
+         *  }
+         */
+        return message;
+    }
 }
