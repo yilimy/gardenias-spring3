acdf867b53e6b97776642acf76f13b996ace5d99
caimeng <caaiimeenngg@163.com>
2025-04-15 16:37:40 +0800

    WebAsyncTask实现异步处理

--- first-micro-boot/first-micro-boot-web/src/main/java/com/gardenia/web/action/MessageAsyncAction.java
+++ first-micro-boot/first-micro-boot-web/src/main/java/com/gardenia/web/action/MessageAsyncAction.java
@@ -3,8 +3,10 @@ package com.gardenia.web.action;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.WebAsyncTask;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 接收一个请求，进行异步处理后，再返回
@@ -29,7 +31,7 @@ public class MessageAsyncAction {
      *     解决方案：
      *      关闭过滤器 {@link com.gardenia.web.filter.MessageFilter}
      * @param msg 请求参数
-     * @return 异步对象
+     * @return 异步Callable对象
      */
     @RequestMapping("/callable")
     public Object echo(String msg) {
@@ -44,5 +46,33 @@ public class MessageAsyncAction {
             log.info("【内部线程】:{}", Thread.currentThread().getName());
             return "【ECHO】" + msg;  // 数据的响应
         };
+    }
+
+    /**
+     * 通过WebAsyncTask进行异步处理
+     * @param msg 请求参数
+     * @return 异步任务对象 WebAsyncTask
+     */
+    @RequestMapping("/webAsyncTask")
+    public Object webAsyncTask(String msg) {
+        // 2025-04-15 16:35:31.370 [http-nio-8080-exec-2] - INFO [] c.g.web.action.MessageAsyncAction - 【外部线程】:http-nio-8080-exec-2
+        log.info("【外部线程】:{}", Thread.currentThread().getName());
+        Callable<String> callable = () -> {
+            // 2025-04-15 16:35:31.372 [async-thread-6] - INFO [] c.g.web.action.MessageAsyncAction - 【内部线程】:async-thread-6
+            log.info("【内部线程】:{}", Thread.currentThread().getName());
+            // 模拟操作的延时
+            // 当前的操作一定会超时
+            // 2025-04-15 16:35:32.023 [http-nio-8080-exec-3] -ERROR [] c.g.web.advice.GlobalExceptionAdvice - 【全局异常】捕获到异常，异常信息: sleep interrupted
+            TimeUnit.SECONDS.sleep(1);
+            return "【ECHO】" + msg;  // 数据的响应
+        };
+        // 200毫秒内完成异步处理
+        WebAsyncTask<String> webAsyncTask = new WebAsyncTask<>(200, callable);
+        webAsyncTask.onTimeout(() -> {
+            // 2025-04-15 16:35:31.372 [async-thread-6] - INFO [] c.g.web.action.MessageAsyncAction - 【内部线程】:async-thread-6
+            log.info("【超时】:{}", Thread.currentThread().getName());
+            return "【超时】" + msg;
+        });
+        return webAsyncTask;
     }
 }
